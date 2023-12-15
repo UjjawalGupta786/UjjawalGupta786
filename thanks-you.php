84114294758af33f632cebc8342b54a828ef42ed
UjjawalGupta786 <90521194+UjjawalGupta786@users.noreply.github.com>
2023-12-15 23:55:36 +0530

    Add files via upload

---
+++ thanks-you.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Thank You</title>
+  <style>
+    /* CSS styles for the thank you page */
+    .container {
+      max-width: 400px;
+      margin: 0 auto;
+      padding: 20px;
+      background-color: #f2f2f2;
+      border-radius: 5px;
+      text-align: center;
+    }
+
+    .thankyou-message {
+      font-size: 24px;
+      margin-bottom: 20px;
+    }
+  </style>
+</head>
+<body>
+  <div class="container">
+    <h1>Thank You for Your Purchase!</h1>
+    <p class="thankyou-message">We appreciate your business.</p>
+    <p>For any further assistance, please contact our support team.</p>
+  </div>
+</body>
+</html>
